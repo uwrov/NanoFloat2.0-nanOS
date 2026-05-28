49a660f9d365d0c49c971d5cbe6b7c886e47da26
kimsukat <abiramiksub@gmail.com>
2026-05-28 15:24:03 -0700

    Fix typo in piston_out function declaration

--- MATE-Competition/MATE-Task-Transmitter.ino
+++ MATE-Competition/MATE-Task-Transmitter.ino
@@ -363,7 +363,7 @@ void set_time_manually() {
 // //================================================================================================================================================
 // //                                                              Piston Control Functions
 
-vvoid piston_out() {
+void piston_out() {
   mcp.digitalWrite(PIN_MOTOR_1, LOW);
   mcp.digitalWrite(PIN_MOTOR_2, LOW);
 
