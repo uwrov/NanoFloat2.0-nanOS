3bae2501807c733e6fe0a762b6af86d46a02c1ff
kimsukat <abiramiksub@gmail.com>
2026-06-06 12:36:48 -0700

    Add configuration constants for task parameters

--- MATE-Competition/taskconfig.h
+++ MATE-Competition/taskconfig.h
@@ -1,1 +1,12 @@
+#ifndef TASKCONFIG_H
+#define TASKCONFIG_H
 
+const long ENCODER_MIN_COUNT = 0;
+const long ENCODER_MAX_COUNT = 200000;
+
+const float MIN_DEPTH_M = 0.0;
+const float MAX_DEPTH_M = 4.0;
+
+const unsigned long MAX_MOTOR_TIME_MS = 240000; // In milliseconds - 4 minutes
+
+#endif
