51fd4b0ea103373dc2b83f991edeebe2181d47bd
kimsukat <abiramiksub@gmail.com>
2026-06-17 14:45:52 -0700

    Adjust ENCODER_MAX_COUNT to 195000

--- MATE-Competition/taskconfig.h
+++ MATE-Competition/taskconfig.h
@@ -2,7 +2,7 @@
 #define TASKCONFIG_H
 
 const long ENCODER_MIN_COUNT = 0;
-const long ENCODER_MAX_COUNT = 200000;
+const long ENCODER_MAX_COUNT = 195000;
 
 const float MIN_DEPTH = 0.0;
 const float MAX_DEPTH = 4.0;
